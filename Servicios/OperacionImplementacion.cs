0acdf16709a4996fa9f9d0b0aaa8d49e843b437c
DESKTOP-2U1CBE1\vspc <csi23-jpribio@colegioaltair.net>
2023-10-31 16:22:46 +0100

    depurador bucle

--- Servicios/OperacionImplementacion.cs
+++ Servicios/OperacionImplementacion.cs
@@ -26,108 +26,120 @@ namespace EjercicioFinalBucleC.Servicios
                 }
                 else
                 {
-                    if (mes == 2 & año % 4 == 0)
+                    if (mes == 2 & año % 4 != 0)
                     {
-                        Console.WriteLine("Este año es bisiesto y el mes es Febrero, lo cual tiene 29 dias");
+                        Console.WriteLine("Este año no es bisiesto y el mes es Febrero, lo cual tiene 28 dias");
                         Console.WriteLine("Y el año se trata de:" + año+"\n");
                     }
                     else
                     {
-                        if (mes == 2 && año % 4 != 0)
+                        if(mes==2 & año % 100 != 0)
                         {
-                            Console.WriteLine("Este año no es bisiesto y el mes es Febrero lo cual tiene 28 dias");
+                            Console.WriteLine("Este año es bisiesto y el mes es Febrero, lo cual tiene 29 dias");
                             Console.WriteLine("Y el año se trata de:" + año + "\n");
                         }
                         else
                         {
-                            if (mes == 3)
+                            if(mes==2 & año % 400 != 0)
                             {
-                                Console.WriteLine("Este mes se trata de Marzo y tiene 31 dias ");
+                                Console.WriteLine("Este año no es bisiesto y el mes es Febrero, lo cual tiene 28 dias");
                                 Console.WriteLine("Y el año se trata de:" + año + "\n");
                             }
                             else
                             {
-                                if (mes == 4)
-                                {
-                                    Console.WriteLine("Este mes se trata de Abril y tiene 30 dias ");
-                                    Console.WriteLine("Y el año se trata de:" + año + "\n");
-                                }
-                                else
-                                {
-                                    if (mes == 5)
+                                Console.WriteLine("Este año es bisiesto y el mes es Febrero, lo cual tiene 29 dias");
+                                Console.WriteLine("Y el año se trata de:" + año + "\n");
+                            }  if (mes == 3)
                                     {
-                                        Console.WriteLine("Este mes se trata de Mayo y tiene 31 dias ");
+                                        Console.WriteLine("Este mes se trata de Marzo y tiene 31 dias ");
                                         Console.WriteLine("Y el año se trata de:" + año + "\n");
                                     }
                                     else
                                     {
-                                        if (mes == 6)
+                                        if (mes == 4)
                                         {
-                                            Console.WriteLine("Este mes se trata de Junio y tiene 30 dias ");
+                                            Console.WriteLine("Este mes se trata de Abril y tiene 30 dias ");
                                             Console.WriteLine("Y el año se trata de:" + año + "\n");
                                         }
                                         else
                                         {
-                                            if (mes == 7)
+                                            if (mes == 5)
                                             {
-                                                Console.WriteLine("Este mes se trata de Julio y tiene 31 dias ");
+                                                Console.WriteLine("Este mes se trata de Mayo y tiene 31 dias ");
                                                 Console.WriteLine("Y el año se trata de:" + año + "\n");
                                             }
                                             else
                                             {
-                                                if (mes == 8)
+                                                if (mes == 6)
                                                 {
-                                                    Console.WriteLine("Este mes se trata de Agosto y tiene 31 dias ");
+                                                    Console.WriteLine("Este mes se trata de Junio y tiene 30 dias ");
                                                     Console.WriteLine("Y el año se trata de:" + año + "\n");
                                                 }
                                                 else
                                                 {
-                                                    if (mes == 9)
+                                                    if (mes == 7)
                                                     {
-                                                        Console.WriteLine("Este mes se trata de Sepetiembre y tiene 30 dias ");
+                                                        Console.WriteLine("Este mes se trata de Julio y tiene 31 dias ");
                                                         Console.WriteLine("Y el año se trata de:" + año + "\n");
                                                     }
                                                     else
                                                     {
-                                                        if (mes == 10)
+                                                        if (mes == 8)
                                                         {
-                                                            Console.WriteLine("Este mes se trata de Octubre y tiene 31 dias ");
+                                                            Console.WriteLine("Este mes se trata de Agosto y tiene 31 dias ");
                                                             Console.WriteLine("Y el año se trata de:" + año + "\n");
                                                         }
                                                         else
                                                         {
-                                                            if (mes == 11)
+                                                            if (mes == 9)
                                                             {
-                                                                Console.WriteLine("Este mes se trata de Noviembre y tiene 30 dias ");
+                                                                Console.WriteLine("Este mes se trata de Sepetiembre y tiene 30 dias ");
                                                                 Console.WriteLine("Y el año se trata de:" + año + "\n");
                                                             }
                                                             else
                                                             {
-                                                                if (mes == 12)
+                                                                if (mes == 10)
                                                                 {
-                                                                    Console.WriteLine("Este mes se trata de Diciembre y tiene 31 dias ");
+                                                                    Console.WriteLine("Este mes se trata de Octubre y tiene 31 dias ");
                                                                     Console.WriteLine("Y el año se trata de:" + año + "\n");
                                                                 }
                                                                 else
                                                                 {
-                                                                    if (mes < 1 & mes > 12)
+                                                                    if (mes == 11)
                                                                     {
-                                                                        Console.WriteLine("Error en el sistema, este mes no existe");
+                                                                        Console.WriteLine("Este mes se trata de Noviembre y tiene 30 dias ");
+                                                                        Console.WriteLine("Y el año se trata de:" + año + "\n");
+                                                                    }
+                                                                    else
+                                                                    {
+                                                                        if (mes == 12)
+                                                                        {
+                                                                            Console.WriteLine("Este mes se trata de Diciembre y tiene 31 dias ");
+                                                                            Console.WriteLine("Y el año se trata de:" + año + "\n");
+                                                                        }
+                                                                        else
+                                                                        {
+                                                                            if (mes < 1 & mes > 12)
+                                                                            {
+                                                                                Console.WriteLine("Error en el sistema, este mes no existe");
+                                                                            }
+                                                                        }
                                                                     }
                                                                 }
                                                             }
                                                         }
                                                     }
                                                 }
+
                                             }
                                         }
-
                                     }
                                 }
                             }
                         }
-                    }
-                }
+                    
+                  
+                
 
                  Console.WriteLine("¿Quieres continuar?Elija la opcion s(si quieres avanzar)/n(si no quieres avanzar)");
                 afirmacion = Console.ReadLine();  
